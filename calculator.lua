f94ff12f93bb8e1283b809e21068f98f02483975
Evan Gipson <evangipson@gmail.com>
2026-02-08 16:44:19 -0600

    Refactor operations into table for clarity

--- calculator.lua
+++ calculator.lua
@@ -1,24 +1,21 @@
-local Operations = require ("operations")
+local Operations = require("operations")
+
+local all_operations = Operations.get_all_operations()
 
 local function main_menu()
-  local should_exit = false
+  -- a rolling total
   local output = 0
-  while not Operations.exit_application(should_exit) do
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-    print("5. Square")
-    print("6. Square Root")
-    print("9. Clear")
-    print("0. Quit")
-    local number = io.read()
-    if number == "0" then
-      should_exit = true
-    elseif number ~= nil then
-      output = Operations.do_operation(number, output)
-      print("your result is " .. (output or "NaN"))
+
+  -- loop calculator input always unless exiting
+  while true do
+    -- print out all operations for the user
+    for op, op_data in ipairs(all_operations) do
+      print(op..". "..op_data.name)
     end
+    -- read the operation requested, then perform it
+    local number = io.read()
+    output = Operations.do_operation(number, output)
+    print("your result is " .. (output or "NaN"))
   end
 end
 
--- operations.lua
+++ operations.lua
@@ -1,82 +1,97 @@
 local Operations = {}
 
+-- A collection of all keys for operations
+local operation_keys = {
+    ADD = 1,
+    SUBTRACT = 2,
+    MULTIPLY = 3,
+    DIVIDE = 4,
+    SQUARE = 5,
+    SQUARE_ROOT = 6,
+    CLEAR = 7,
+    QUIT = 8,
+}
+
 local function request_numbers(input, output)
     -- if there isnt an output or output is 0, ask for both numbers. Otherwise, ask only for 1 to do OPERATION with OUTPUT
-    if not output or output == 0 then 
+    if not output or output == 0 then
         print("Enter number")
     end
     -- assign output to n1 as long as it doesnt = 0. if 0 then use io.read()
     local n1 = output ~= 0 and output or io.read()
-    if input == "5" or input == "6" then
-        local Numbers = { n1 }
-        return Numbers
-    else
-        print("Enter a second number")
-        local n2 = io.read()
-        local Numbers = { n1, n2 }
-        return Numbers
+    if input == operation_keys.SQUARE or input == operation_keys.SQUARE_ROOT then
+        return { n1 }
     end
+
+    print("Enter a second number")
+    local n2 = io.read()
+    return { n1, n2 }
 end
 
 -- Math Functions
 
-local function do_add(output)
-    local result = request_numbers(output)
-    return result[1] + result [2]
+local function do_add(input, output)
+    local result = request_numbers(input, output)
+    return result[1] + result[2]
 end
 
-local function do_sub(output)
-    local result = request_numbers(output)
-    return result[1] - result [2]
+local function do_sub(input, output)
+    local result = request_numbers(input, output)
+    return result[1] - result[2]
 end
 
-local function do_multi(output)
-    local result = request_numbers(output)
-    return result[1] * result [2]
+local function do_multi(input, output)
+    local result = request_numbers(input, output)
+    return result[1] * result[2]
 end
 
-local function do_div(output)
-    local result = request_numbers(output)
-    return result[1] / result [2]
+local function do_div(input, output)
+    local result = request_numbers(input, output)
+    return result[1] / result[2]
 end
 
-local function do_square(output)
-    local result = request_numbers(output)
-    return result[1] * result [1]
+local function do_square(input, output)
+    local result = request_numbers(input, output)
+    return result[1] * result[1]
 end
 
-local function do_square_root(output)
-    local result = request_numbers(output)
+local function do_square_root(input, output)
+    local result = request_numbers(input, output)
     return math.sqrt(result[1])
 end
 
--- Math Functions End
-
-function Operations.exit_application(should_exit)
-  if should_exit then
-    print("Night Night")
-      os.exit()
-  end
+local function do_clear()
+    return 0
 end
 
--- Orchestrator - responsible for choosing the correct functions after receiving input from io.read
+local function do_exit()
+    print("Nighty night")
+    os.exit()
+end
+
+-- A collection of potential operations
+local all_operations = {
+    [operation_keys.ADD] = { name = "add", symbol = "+", min_numbers = 2, behavior = do_add },
+    [operation_keys.SUBTRACT] = { name = "subtract", symbol = "-", min_numbers = 2, behavior = do_sub },
+    [operation_keys.MULTIPLY] = { name = "multiply", symbol = "*", min_numbers = 2, behavior = do_multi },
+    [operation_keys.DIVIDE] = { name = "divide", symbol = "/", min_numbers = 2, behavior = do_div },
+    [operation_keys.SQUARE] = { name = "square", symbol = "^2", min_numbers = 1, behavior = do_square },
+    [operation_keys.SQUARE_ROOT] = { name = "square root", symbol = "√", min_numbers = 1, behavior = do_square_root },
+    [operation_keys.CLEAR] = { name = "clear", symbol = nil, min_numbers = nil, behavior = do_clear },
+    [operation_keys.QUIT] = { name = "quit", symbol = nil, min_numbers = nil, behavior = do_exit },
+}
+
+-- Math Functions End
+
+-- Gets all operations available in the `Operations` module.
+function Operations.get_all_operations()
+    return all_operations
+end
+
+-- Orchestrator that is responsible for choosing the correct functions after receiving input from io.read
 function Operations.do_operation(input, output)
-    if input == "1" then
-        return do_add(output)
-    elseif input == "2" then
-        return do_sub(output)
-    elseif input == "3" then
-        return do_multi(output)
-    elseif input == "4" then
-        return do_div(output)
-    elseif input == "5" then
-        return do_square(output)
-    elseif input == "6" then
-        return do_square_root(output)
-    --Clear
-    elseif input == "9" then
-        return 0
-    end
+    local operation_key = tonumber(input)
+    return all_operations[operation_key] and all_operations[operation_key].behavior(input, output) or 0
 end
 
 return Operations
